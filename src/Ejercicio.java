84e820931d8a828ac806796ebca8245e5ea79fa7
JanoT19 <janotoralsanmiguel2004@gmail.com>
2024-04-17 12:22:56 +0200

    commit de samu el 19 de abril

--- src/Ejercicio.java
+++ src/Ejercicio.java
@@ -1,9 +1,10 @@
-
 public class Ejercicio {
 
 	public static void main(String[] args) {
 		System.out.println("Hola soy Samuel");
-
+		for (int i = 0; i < 10; i++) {
+			System.out.println(2 + " x " + (i+1) + " = " + (i+1)*2);
+		}
 	}
 
 }
